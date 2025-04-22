cf826f7601c2f2e73a963d78281b861936de5efd
psjw <dlrudtn108@gmail.com>
2025-04-22 19:09:11 +0900

    initial commit

---
+++ src/main/java/com/psjw/resilience4jtest/Resilience4jTestApplication.java
@@ -0,0 +1,13 @@
+package com.psjw.resilience4jtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4jTestApplication.class, args);
+    }
+
+}
